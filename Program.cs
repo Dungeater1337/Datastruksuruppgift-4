b2d21653f6448ebec18a37a9d7ebf55daa0aa4bf
vikkan <111962656+Dungeater1337@users.noreply.github.com>
2023-10-18 12:53:25 +0200

    dd
    dd

--- Program.cs
+++ Program.cs
@@ -2,9 +2,37 @@
 
 class Program
 {
-    static int 
+
+    static int AdderaTvåTal(int[] arr)
+    {
+        HashSet<int> numbers = new HashSet<int>();
+        int target = -2;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int comp = target - arr[i];
+
+            if(numbers.Contains(comp))
+            {
+                return 1;
+            }
+
+            numbers.Add(arr[i]);
+        }
+
+        return 0; 
+    }
     static void Main(string[] args)
     {
-
+        int[] arr = new int[]{0, -1, 2, -3, 1};
+        AdderaTvåTal(arr);
+        if(AdderaTvåTal(arr) != 0)
+        {
+            Console.WriteLine("Tal hittades"); 
+        }
+        else
+        {
+            Console.WriteLine("Inga tal hittades");
+        }
     }
 }
